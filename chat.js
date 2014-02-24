f11f7cc649485c24b6fbebcb4417ab151277b565
Kim Hsiao <white.xtera@gmail.com>
2014-02-25 00:55:40 +0800

    1. inital chat.js 2. setup an empty template

--- chat.js
+++ chat.js
@@ -3,18 +3,21 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+  app = express();
+  http = require('http');
+  swig = require('swig'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path');
 
-var app = express();
-
-// all environments
 app.set('port', process.env.PORT || 3000);
+// all environments
+// app.set('port', process.env.PORT || 3000);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -27,12 +30,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
+  app.set('view cache', false);
+  swig.setDefaults({cache: false});
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', {hello: 'world'});
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
