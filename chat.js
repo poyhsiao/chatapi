08a8f5cc7989d7bcb854361831e321cc2c0ef674
Kim Hsiao <white.xtera@gmail.com>
2014-02-25 00:56:44 +0800

    1. remove empty lines

--- chat.js
+++ chat.js
@@ -44,8 +44,6 @@ server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-
 io.on('connection', function (socket) {
   socket.emit('news', {hello: 'world'});
   socket.on('my other event', function (data) {
